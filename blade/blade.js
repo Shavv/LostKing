bb6588f191c090b614e5a8874b493f745cfc0d91
Glenn's Laptop <glentr1@gmail.com>
2017-12-18 22:33:22 +0100

    Added add, check, remove function to inventory

--- blade/blade.js
+++ blade/blade.js
@@ -1,5 +1,6 @@
 //blade.js
 var fs = require('fs');
+var xml2js = require('xml2js');
 
 console.log("LostKing Blade ©2017-2018");
 console.log("Type /help for a list of commands");
@@ -14,10 +15,21 @@ process.stdin.on("data", function(input){
 				break;
 				case "parent":
 					var object_directory = get_parent_directory()+"\\Lostking.gmx\\objects";
-					console.log(object_directory);
-					
+
 					var file_name_list = fs.readdirSync(object_directory);
-					console.log(file_name_list);
+					
+					var i;
+						for(i=0; i<file_name_list.length; i++){
+							var result_file_name = file_name_list[i];
+								if(result_file_name.substring(0, 4)=="item"){
+									var parser = new xml2js.Parser();
+									fs.readFile(object_directory+"\\"+result_file_name, function(err, data) {
+										parser.parseString(data, function (err, result) {
+											console.log(result.object.events[0].event[0].action[0].arguments[0].argument[0].string[0]);
+										});
+									});
+								}
+						}
 				break;
 			}
 		}
--- server/player.js
+++ server/player.js
@@ -30,12 +30,66 @@ lostking.inventory = function(player_object){
 			this.slot_list.push(new lostking.item_slot());
 		}
 	this.add = function(item){
-		
-	}
-	this.equip = function(slot_index){
-		
+		var added = false;
+		var i;
+			for(i=0; i<this.slot_list.length; i++){//Add item to existing stack
+				var result_slot = this.slot_list[i];
+					if(result_slot.item_list.length>0){
+						if(result_slot.item_list[0].blueprint.index==item.blueprint.index){
+							if(result_slot.item_list.length<item.blueprint.max_stack_size){
+								result_slot.item_list.push(item);
+								added = true;
+								break;
+							}
+						}
+					}
+			}
+			if(!added){
+				for(i=0; i<this.slot_list.length; i++){//Add item to existing stack
+					var result_slot = this.slot_list[i];
+						if(result_slot.item_list.length==0){
+							result_slot.item_list.push(item);
+							added = true;
+							break;
+						}
+				}
+			}
+			if(added){
+				return true;
+			}else{
+				return false;
+			}
 	}
 	this.check = function(item, amount){
+		var i;//When stack isn't full add it to item of same type
+			for(i=0; i<this.slot_list.length; i++){
+				var result_slot = this.slot_list[i];
+					if(result_slot.item_list.length>0){
+						if(result_slot.item_list[0].blueprint.index==item.blueprint.index){
+							amount -= result_slot.item_list.length;
+						}
+					}
+			}
+			if(amount>0){
+				return false;
+			}
+		return true;
+	}
+	this.remove = function(item, amount){
+		var i, j;//Remove
+			for(i=this.slot_list.length-1; i>=0; i--){
+				var result_slot = this.slot_list[i];
+					if(result_slot.item_list.length>0 && amount>0){
+						if(result_slot.item_list[0].blueprint.index==item.blueprint.index){
+							var remove_amount = Math.min(amount, result_slot.item_list.length);
+							amount -= remove_amount;
+
+							result_slot.item_list.splice(0, remove_amount);
+						}
+					}
+			}
+	}
+	this.equip = function(slot_index){
 		
 	}
 }
@@ -52,6 +106,13 @@ lostking.equipment = function(player_object){
 lostking.item_slot = function(){
 	this.item_list = [];
 }
-lostking.item = function(type){
-	this.type = type;
+lostking.item_default_blueprint = function(index, name, max_stack_size, rarity){
+	this.index = index;
+	this.name = name;
+	this.max_stack_size = max_stack_size;
+	this.action_index = 0;
+	this.rarity = rarity;
+}
+lostking.item_default = function(blueprint){
+	this.blueprint = blueprint;
 }
